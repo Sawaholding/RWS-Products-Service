87da45f6da5432f9e4d57ab8d5086a1ef9d0f084
NOurkhatib <m.n.khatib2016@gmail.com>
2022-05-22 15:26:48 +0200

    connecting to dbstarting docker prepiration

--- src/main/java/RWS/products/ProductsApplication.java
+++ src/main/java/RWS/products/ProductsApplication.java
@@ -1,15 +1,20 @@
 package RWS.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ProductsApplication {
 
 
-
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsApplication.class, args);
+
 
 	}
 
